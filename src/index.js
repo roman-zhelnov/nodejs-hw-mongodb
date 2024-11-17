855b069ee9526d2ea4730323b03ba238f8967a27
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-11-17 12:37:35 +0200

    add .env

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import 'dotenv/config';
 import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-setupServer();
+const bootstrap = async () => {
+  try {
+    await initMongoConnection();
+    setupServer();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+bootstrap();
